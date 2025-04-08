783a47a9be40f251b3194349938ffea4da5e35cd
mikey-mdk <michael.david.mdk@gmail.com>
2025-04-08 15:49:12 -0600

    Adding rules

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,18 @@
+defmodule Raffley.Rules do
+  def list_rules do
+    [
+      %{
+        id: 1,
+        text: "Participants must have a high tolerance for puns and dad jokes."
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance"
+      },
+      %{
+        id: 3,
+        text: "Winner must be named Michael"
+      }
+    ]
+  end
+end
